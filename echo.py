335d00ae81d51ffce89fc6318ecefa0cc0ffffe5
12189 <bipt2021@gmail.com>
2025-10-21 08:31:07 +0800

    refactor: change prompt to 'Say something' in master

--- echo.py
+++ echo.py
@@ -1,7 +1,7 @@
 import sys
 def echo():
     shout = "-s" in sys.argv  # 检测是否传入 -s 参数
-    message = input("Enter something: ")
+    message = input("Say something: ")  # master 分支的修改
     print(message.upper() if shout else message)  # 大写输出
 if __name__ == "__main__":
     echo()
